bbeba0e9ac09e4638998f56697ac1d7fb7331b2e
Tushar Sanchaniya <tushar.sanchaniya@mindinventory.com>
2024-04-11 15:03:05 +0530

    feat(main): remove warnings

--- AnimatedCurveBottomTabbar/Classes/Utils/Color+Extension.swift
+++ AnimatedCurveBottomTabbar/Classes/Utils/Color+Extension.swift
@@ -5,36 +5,24 @@ import SwiftUI
 extension Color {
     
     public static func colorWithHexString(hexString: String, withAlpha: CGFloat = 1) -> Color {
-        var colorString = hexString.trimmingCharacters(in: .whitespacesAndNewlines)
-        colorString = colorString.replacingOccurrences(of: "#", with: "").uppercased()
+        var formattedHexString = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
         
-        if colorString.count != 6 {
+        if formattedHexString.hasPrefix("#") {
+            formattedHexString.remove(at: formattedHexString.startIndex)
+        }
+        
+        if formattedHexString.count != 6 {
             return Color.black
         }
         
-        let alpha: CGFloat = withAlpha
-        let red: CGFloat = colorComponentFrom(colorString: colorString, start: 0, length: 2)
-        let green: CGFloat = colorComponentFrom(colorString: colorString, start: 2, length: 2)
-        let blue: CGFloat = colorComponentFrom(colorString: colorString, start: 4, length: 2)
-        
-        let color = Color(red: red, green: green, blue: blue).opacity(alpha)
-        return color
-    }
-    
-    fileprivate static func colorComponentFrom(colorString: String, start: Int, length: Int) -> CGFloat {
-        
-        let startIndex = colorString.index(colorString.startIndex, offsetBy: start)
-        let endIndex = colorString.index(startIndex, offsetBy: length)
-        let subString = colorString[startIndex..<endIndex]
-        let fullHexString = length == 2 ? subString : "\(subString)\(subString)"
-        var hexComponent: UInt32 = 0
-        
-        guard Scanner(string: String(fullHexString)).scanHexInt32(&hexComponent) else {
-            return 0
+        guard formattedHexString.count == 6, let rgbValue = UInt64(formattedHexString, radix: 16) else {
+            return Color.black
         }
-        let hexFloat: CGFloat = CGFloat(hexComponent)
-        let floatValue: CGFloat = CGFloat(hexFloat / 255.0)
-        return floatValue
+        
+        let red = Double((rgbValue & 0xFF0000) >> 16) / 255.0
+        let green = Double((rgbValue & 0x00FF00) >> 8) / 255.0
+        let blue = Double(rgbValue & 0x0000FF) / 255.0
+        
+        return Color(red: red, green: green, blue: blue)
     }
-
 }
